68c3f99fe6a754519156a8d1e16f48ba0993d47c
K Haridass, Thinagaran <Thinagaran.KHaridass@sicpa.com>
2018-11-13 17:46:35 +0800

    Upgraded to angular 7 and included angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule, APP_INITIALIZER  } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {  HttpClientModule} from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { CreateUserComponent } from './create-user/create-user.component';
 import { CreateUserFormComponent } from './create-user-form/create-user-form.component';
@@ -12,7 +11,11 @@ import { CreateRouteComponent } from './create-route/create-route.component';
 import { SubscribeComponent } from './subscribe/subscribe.component';
 import {KeycloakHttp, keycloakHttpFactory} from "./keycloak/keycloak.http";
 import {HttpModule, RequestOptions, XHRBackend} from "@angular/http";
+
 import {KeycloakService} from "./keycloak/keycloak.service";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+
 
 const appRoutes: Routes = [
   { path: 'create-user' , component: CreateUserComponent}
@@ -35,7 +38,14 @@ const appRoutes: Routes = [
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
-    )
+    ),
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatCheckboxModule
+  ],
+  exports: [
+    MatButtonModule, 
+    MatCheckboxModule
   ],
   providers: [ 
       {
